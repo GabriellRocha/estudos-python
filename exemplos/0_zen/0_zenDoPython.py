8aad38ed20d61fbc9a50cf068ccab867264fd36d
Gabriel Rocha <104218940+GabriellRocha@users.noreply.github.com>
2022-09-29 08:53:40 -0300

    new content

--- exemplos/0_zen/0_zenDoPython.py
+++
@@ -1,6 +0,0 @@
-import this
-
-'''A filosofia da comunidade Python está contida no “Zen de Python”.
-Você pode acessar esse conjunto resumido de princípios para
-escrever um bom código Python'''
-print(this)
